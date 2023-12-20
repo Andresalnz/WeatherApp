a08621a4b061d8914b72a3cb47c9b5015ce1c7de
Andresalnz <andresaleunz@gmail.com>
2023-12-20 13:03:00 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Andres Aleu on 20/12/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
