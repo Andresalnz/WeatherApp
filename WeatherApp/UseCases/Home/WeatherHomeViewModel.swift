53268aa1bd67c03b66163120693d2e189785a209
Andresalnz <andresaleunz@gmail.com>
2024-01-17 11:35:22 +0100

    Add Location in ViewModel

--- WeatherApp/UseCases/Home/WeatherHomeViewModel.swift
+++ WeatherApp/UseCases/Home/WeatherHomeViewModel.swift
@@ -6,26 +6,39 @@
 //
 
 import Foundation
+import CoreLocation
+import SwiftUI
 
-final class WeatherHomeViewModel: ObservableObject {
+final class WeatherHomeViewModel: NSObject, ObservableObject {
     
+    //MARK: - Interactor
     private let interactor: Interactor
-    
     @Published var actualWeather: CurrentWeatherBO = CurrentWeatherBO(id: 1)
     
+    //MARK: - Varaibles Location
+    private let locationManager: CLLocationManager = CLLocationManager()
+    var location: CLLocation?
+    
+    //MARK: - Init
     init(interactor: Interactor = Interactor.shared) {
         self.interactor = interactor
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
     }
     
-    func loadUI() {
+    //MARK: - loadUI
+    func loadUI(latitude: CLLocationDegrees, longitude: CLLocationDegrees) {
         Task {
-             await loadData()
+            await loadData(latitude: latitude, longitude: longitude)
         }
     }
     
-    func loadData() async {
+    //MARK: - loadData
+    func loadData(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async {
         do {
-            let weather = try await interactor.getWeather()
+            let weather = try await interactor.getWeather(longitude: longitude, latitude: latitude)
             await MainActor.run {
                 self.actualWeather = weather.toBo()
             }
@@ -33,7 +46,34 @@ final class WeatherHomeViewModel: ObservableObject {
             print("Error \(err)")
         }
     }
+}
+
+//MARK: - CLLocationManagerDelegate
+extension WeatherHomeViewModel:  CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation])  {
+        guard let location = locations.first else { return print("EROR") }
+        self.location = location
+        print("location \(location)")
+        loadUI(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+        self.locationManager.stopUpdatingLocation()
+    }
     
-    
-    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager)  {
+        let status = manager.authorizationStatus
+        switch status {
+            case .notDetermined:
+                manager.requestWhenInUseAuthorization()
+                break
+            case .restricted, .denied:
+                break
+            case .authorizedAlways:
+                manager.startUpdatingLocation()
+                break
+            case .authorizedWhenInUse:
+                manager.startUpdatingLocation()
+                break
+            @unknown default:
+                break
+        }
+    }
 }
