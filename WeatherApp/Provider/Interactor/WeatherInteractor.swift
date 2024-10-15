bf1f16635d7e6a05a022f058f3f6608b328e137c
Andresalnz <andresaleunz@gmail.com>
2024-10-15 13:10:31 +0200

    Edit Interactor

--- WeatherApp/Provider/Interactor/WeatherInteractor.swift
+++ WeatherApp/Provider/Interactor/WeatherInteractor.swift
@@ -10,28 +10,18 @@ import CoreLocation
 
 //MARK: - WeatherInteractor
 protocol WeatherInteractor {
-    func getWeather(longitude: CLLocationDegrees, latitude: CLLocationDegrees) async throws -> CurrentWeatherDTO
+    func getWeather(url: URL?, longitude: CLLocationDegrees, latitude: CLLocationDegrees) async throws -> CurrentWeatherDTO
+    var repository: WeatherRepository { get }
 }
 
 struct Interactor: WeatherInteractor {
-    
-    static let shared = Interactor()
-    
-    func getJSON<T>(url: URL?, type: T.Type) async throws -> T where T: Codable {
-        guard let url = url else { throw fatalError() }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let wheaterModel = try JSONDecoder().decode(type, from: data)
-            print(wheaterModel)
-            return wheaterModel
-        } catch let err  {
-            throw err
-        }
-    }
+   
+    var repository: WeatherRepository
+    static let shared = Interactor(repository: WeatherRepository())
     
     //MARK: - Get location with longitude and latitude
-    func getWeather(longitude: CLLocationDegrees, latitude: CLLocationDegrees) async throws -> CurrentWeatherDTO {
-        return try await getJSON(url: URL(string: "\(Constants.uri + Constants.latitude)\(latitude)\(Constants.longitude)\(longitude)\(Constants.apiKey + Constants.units)")!, type: CurrentWeatherDTO.self)
+    func getWeather(url: URL?, longitude: CLLocationDegrees, latitude: CLLocationDegrees) async throws -> CurrentWeatherDTO {
+        return try await repository.getJSON(url: url, type: CurrentWeatherDTO.self)
     }
 }
 
