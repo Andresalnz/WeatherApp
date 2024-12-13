e09d2281eb164ef450f6e506cf953d12b283e6a2
Andresalnz <andresaleunz@gmail.com>
2024-12-13 17:09:51 +0100

    Add Interactor. Use case, Main Weather

---
+++ WeatherApp/UseCases/MainWeather/Interactor/Interactor.swift
@@ -0,0 +1,32 @@
+//
+//  MainWeatherInteractor.swift
+//  WeatherApp
+//
+//  Created by Andres Aleu on 13/12/24.
+//
+
+import Foundation
+
+extension Interactor {
+    func getCurrentWeather() async throws -> CurrentWeatherDTO {
+        var components = URLComponents(string: baseUrl.absoluteString)!
+        components.queryItems = [
+            URLQueryItem(name: "lat", value: "40.41"),
+            URLQueryItem(name: "lon", value: "-3.70"),
+            URLQueryItem(name: "appid", value: "45cdb8986f5dc87e1b08bb64678efc0e")
+        ]
+        let url = components.url!
+        return try await repository.getJSON(url: url, type: CurrentWeatherDTO.self)
+    }
+}
+
+extension Interactor  {
+    func getCurrentWeatherTest() async throws -> CurrentWeatherDTO {
+        return try await repository.getJSON(url: baseUrl, type: CurrentWeatherDTO.self)
+    }
+}
+
+
+
+//?lat=40.41&lon=-3.70&appid=45cdb8986f5dc87e1b08bb64678efc0e
+
