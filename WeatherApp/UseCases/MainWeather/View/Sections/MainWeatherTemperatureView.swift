7fa72c4e8840fe56f63da30550d2c40ecc0003bf
Andresalnz <andresaleunz@gmail.com>
2024-12-18 14:42:02 +0100

    Add view Temperature

---
+++ WeatherApp/UseCases/MainWeather/View/Sections/MainWeatherTemperatureView.swift
@@ -0,0 +1,37 @@
+//
+//  MainWeatherTemperatureView.swift
+//  WeatherApp
+//
+//  Created by Andres Aleu on 16/12/24.
+//
+
+import SwiftUI
+
+struct MainWeatherTemperatureView: View {
+    
+    var infoTemperature: CurrentWeatherBO
+    
+    var body: some View {
+        VStack {
+            Text(infoTemperature.name ?? "Madrid")
+                .font(.title3)
+                .padding(.top)
+            Text("\(infoTemperature.weatherMain?.temp ?? 20.0, specifier: "%.0fº")")
+                .font(.title)
+            HStack {
+                Text(infoTemperature.weather?.first?.main ?? "")
+                    .font(.title3)
+            }
+            HStack {
+                Text("Maxima. \(infoTemperature.weatherMain?.tempMax ?? 20.0, specifier: "%.0fº")")
+                Text("Minima. \(infoTemperature.weatherMain?.tempMin ?? 20.0, specifier: "%.0fº") ")
+            }
+            .font(.headline)
+        }
+        .padding(.horizontal)
+    }
+}
+
+#Preview {
+    MainWeatherTemperatureView(infoTemperature: .preview)
+}
