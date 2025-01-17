6410961ae97b5ddc2f9a6642c06dd7cbbe5031d2
Andresalnz <andresaleunz@gmail.com>
2025-01-18 00:47:49 +0100

    Remove Vstack

--- WeatherApp/UseCases/MainWeather/View/Sections/MainWeatherTemperatureView.swift
+++ WeatherApp/UseCases/MainWeather/View/Sections/MainWeatherTemperatureView.swift
@@ -12,23 +12,22 @@ struct MainWeatherTemperatureView: View {
     var infoTemperature: CurrentWeatherBO
     
     var body: some View {
-        VStack {
-            Text(infoTemperature.name ?? "")
+        
+        Text(infoTemperature.name ?? "")
+            .font(.title3)
+            .padding(.top)
+        Text("\(infoTemperature.weatherMain?.temp ?? 20.0, specifier: "%.0fº")")
+            .font(.title)
+        HStack {
+            Text(infoTemperature.weather?.first?.main ?? "")
                 .font(.title3)
-                .padding(.top)
-            Text("\(infoTemperature.weatherMain?.temp ?? 20.0, specifier: "%.0fº")")
-                .font(.title)
-            HStack {
-                Text(infoTemperature.weather?.first?.main ?? "")
-                    .font(.title3)
-            }
-            HStack {
-                Text("Maxima. \(infoTemperature.weatherMain?.tempMax ?? 0.0, specifier: "%.0fº")")
-                Text("Minima. \(infoTemperature.weatherMain?.tempMin ?? 0.0, specifier: "%.0fº") ")
-            }
-            .font(.headline)
         }
-        .padding(.horizontal)
+        HStack {
+            Text("Maxima. \(infoTemperature.weatherMain?.tempMax ?? 0.0, specifier: "%.0fº")")
+            Text("Minima. \(infoTemperature.weatherMain?.tempMin ?? 0.0, specifier: "%.0fº") ")
+        }
+        .font(.headline)
+        
     }
 }
 
