de180c997005161c3383500a1e0cbc9d1f71d412
Andresalnz <andresaleunz@gmail.com>
2025-01-20 18:20:58 +0100

    Edit @main

--- WeatherApp/WeatherAppApp.swift
+++ WeatherApp/WeatherAppApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct WeatherAppApp: App {
     var body: some Scene {
         WindowGroup {
-            MainWeatherView(vm: MainWeatherVM(locationManager: CoreLocationManager()))
+            //MainWeatherView(vm: MainWeatherVM(locationManager: CoreLocationManager()))
+            TabBarView()
+                
+               
         }
     }
 }
