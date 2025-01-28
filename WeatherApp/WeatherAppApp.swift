7ab1ce6f250521ce862e1cd91511d68352dd96a6
Andresalnz <andresaleunz@gmail.com>
2025-01-28 23:06:47 +0100

    Add container in main

--- WeatherApp/WeatherAppApp.swift
+++ WeatherApp/WeatherAppApp.swift
@@ -6,13 +6,28 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct WeatherAppApp: App {
+    
+    var container: ModelContainer {
+        let schema = Schema([CityDataModel.self])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        do {
+            
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            
+        } catch let err {
+            print("Error container | \(err.localizedDescription)")
+            fatalError()
+        }
+    }
+        
     var body: some Scene {
         WindowGroup {
             TabBarView()
-                .modelContainer(CityDatabase().container)
+                .modelContainer(container)
         }
     }
 }
