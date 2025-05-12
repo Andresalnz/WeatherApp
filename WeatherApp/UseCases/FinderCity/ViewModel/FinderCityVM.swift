934a2833d10eaac9ec1e29f5752f263143fb9715
Andresalnz <andresaleunz@gmail.com>
2025-05-12 23:16:26 +0200

    Edit ViewModel FidenrCity

--- WeatherApp/UseCases/FinderCity/ViewModel/FinderCityVM.swift
+++ WeatherApp/UseCases/FinderCity/ViewModel/FinderCityVM.swift
@@ -11,50 +11,26 @@ import Combine
 class FinderCityVM: ObservableObject {
     
     private let interactor: Interactor
-    private let database: CityDatabaseProtocol
-    private var cancellables = Set<AnyCancellable>()
-    private var cancellable = Set<AnyCancellable>()
-    var form: String = ""
-    @Published var state: String? = nil
-    @Published var country: String? = nil
+    //private let database: CityDatabaseProtocol
     
+    private var cancellable = Set<AnyCancellable>()
+    
+    @Published var city: GeoCodingElementBO? = nil
     @Published var searchText: String
-    @Published var cityItem: String? = nil
     @Published var cities: [GeoCodingElementBO]
     @Published var searchCityWeather: CurrentWeatherBO
     
-    init(interactor: Interactor = Weather(repository: Repository()), database: CityDatabaseProtocol, searchText: String = "", cities: [GeoCodingElementBO] = [], searchCityWeather: CurrentWeatherBO = CurrentWeatherBO(id: 1)) {
+    init(interactor: Interactor = Weather(repository: Repository()), /*database: CityDatabaseProtocol,*/ searchText: String = "", cities: [GeoCodingElementBO] = [], searchCityWeather: CurrentWeatherBO = CurrentWeatherBO(id: 1)) {
         self.interactor = interactor
-        self.database = database
+        //self.database = database
         self.searchText = searchText
         self.cities = cities
         self.searchCityWeather = searchCityWeather
     }
     
     func loadDataCities() async {
-            $searchText
-                //.debounce(for: .seconds(2), scheduler: DispatchQueue.main)
-                .sink(receiveCompletion: { completion in
-                    switch completion {
-                        case .finished:
-                            print("Final")
-                        case .failure(_):
-                            print("Error completion")
-                    }
-                }, receiveValue: { [weak self] searchCity in
-                    guard let wSelf = self else { return }
-                    Task {
-                        let searchCities = try await wSelf.interactor.getFinderCities(from: searchCity)
-                        await MainActor.run {
-                            
-                            wSelf.cities = searchCities.map({$0.toBo()})
-                            print(wSelf.cities)
-                        }
-                    }
-                })
-                .store(in: &cancellables)
-        
-        $cityItem
+        $searchText
+            .debounce(for: .seconds(2), scheduler: DispatchQueue.main)
             .sink(receiveCompletion: { completion in
                 switch completion {
                     case .finished:
@@ -62,25 +38,43 @@ class FinderCityVM: ObservableObject {
                     case .failure(_):
                         print("Error completion")
                 }
-            }, receiveValue: { [weak self] i in
+            }, receiveValue: { [weak self] searchCity in
                 guard let wSelf = self else { return }
                 Task {
-                    if let i = i {
-                        wSelf.form = [i, wSelf.state, wSelf.country].compactMap({$0}).joined(separator: ", ")
-                        let actualSearchCityWeather = try await wSelf.interactor.getSearchCity(nameCity: wSelf.form)
+                    let searchCities = try await wSelf.interactor.getFinderCities(from: searchCity)
+                    await MainActor.run {
+                        wSelf.cities = searchCities.map({$0.toBo()})
+                    }
+                }
+            })
+            .store(in: &cancellable)
+        
+        $city
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                    case .finished:
+                        print("Final")
+                    case .failure(_):
+                        print("Error completion")
+                }
+            }, receiveValue: { [weak self] city in
+                guard let wSelf = self else { return }
+                if let city = city {
+                    Task {
+                        
+                        let actualSearchCityWeather = try await wSelf.interactor.getSearchCity(lat: city.lat ?? 0.0, long: city.lon ?? 0.0)
                         await MainActor.run {
                             wSelf.searchCityWeather = actualSearchCityWeather.toBo()
                         }
                     }
-                   
                 }
             })
             .store(in: &cancellable)
     }
     
-    func saveCity() async {
-        let city: CityDataModel = CityDataModel(nameCity: searchCityWeather.name ?? "", stateCity: state, countryCity: country, temperature: searchCityWeather.weatherMain?.temp ?? 0.0, temperatureMax: searchCityWeather.weatherMain?.tempMax ?? 0.0, temperatureMin: searchCityWeather.weatherMain?.tempMin ?? 0.0 , stateSky: searchCityWeather.weather?.first?.main ?? "", dt: searchCityWeather.dt ?? "")
-        
-        await database.saveCity(city)
-    }
+    //    func saveCity() async {
+    //        let city: CityDataModel = CityDataModel(nameCity: searchCityWeather.name ?? "", stateCity: state, countryCity: country, temperature: searchCityWeather.weatherMain?.temp ?? 0.0, temperatureMax: searchCityWeather.weatherMain?.tempMax ?? 0.0, temperatureMin: searchCityWeather.weatherMain?.tempMin ?? 0.0 , stateSky: searchCityWeather.weather?.first?.main ?? "", dt: searchCityWeather.dt ?? "")
+    //
+    //        await database.saveCity(city)
+    //    }
 }
